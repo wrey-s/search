a9a20750737c48caf3d2fc4eeef0f3b712cc6ba0
wrey_s <reihane.sargaziii@gmail.com>
2024-06-08 22:23:27 -0700

    search time feature added to project

--- search_engine.py
+++ search_engine.py
@@ -5,30 +5,30 @@ Created on Sat Jun  1 10:11:35 2024
 @author: wrey
 """
 import os
+import time
 
-#function for search
-def search(files, search_box):
-    #access and list files
-    for file in os.listdir(files):
-        #counter
-        i=1 
-        with open(os.path.join(files, file), 'r', encoding='utf-8') as filee:
-            for str in filee:
-                if search_box in (str:=str.title()):
-                    print(f'result:{str}*****found in {file}*****line={i}')
-                i+=1
-    print()            
-    print('end of search')
-    print()
-    
 while True:
-    #box for search
+    def search(files, search_box): # تابع برای سرچ
+        global start_time
+        start_time = time.time() #زمان شروع اجرا 
+        for file in os.listdir(files): # دسترسی و لیست فایل ها    
+            i=1 #شمارنده
+            with open(os.path.join(files, file), 'r', encoding='utf-8') as filee:
+                for str in filee:
+                    if search_box in (str:=str.title()):
+                        print(f'result:{str}*****found in {file}*****line={i}')
+                    i+=1
+        print()            
+        print('end of search')
+        
+    #باکس سرچ    
     search_box = input('enter a word for search:  ')
-    #file address
+    #آدرس فایل    
     files = 'data/dickens'
-    #recall function
+    #فراخوانی تابع    
     search(files , (search_box:=search_box.title()))
-        
-           
-        
-        
+    
+    end_time = time.time()  # زمان پایان اجرا
+    execution_time = end_time - start_time  # زمان اجرا
+    print(f"Search time: {execution_time:.2f} seconds")
+    print()
